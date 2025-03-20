ef44271e799dda8853f5213394eefd29f3f1b8a6
lucifer <dawn_alucard@outlook.com>
2025-03-21 01:13:49 +0800

    添加用户配置文件

---
+++ library/user.py
@@ -0,0 +1,19 @@
+class FeiShu:
+    """ 飞书app """
+    APP_ID = 'your_app_id'
+    APP_SECRET = 'your_app_secret'
+
+
+class MySQL:
+    """个人mysql"""
+    type = 'MySQL'
+    user = 'your_username'
+    password = r'your_password'
+    ip = 'localhost:3306'
+    database = 'your_database'
+
+
+class qanwen_manual:
+    """ 千问大模型 """
+    API_KEY = "your_api_key"
+    APP_ID = "your_app_id"
